0c83c55302185f3b2f9a23eb70a12d8e6846b142
Hunter Harloff <harloff@umich.edu>
2020-12-27 21:38:10 -0500

    Serializer addition

--- src/NeuralNet.h
+++ src/NeuralNet.h
@@ -14,14 +14,22 @@ namespace PyreNet {
     public:
         // # of input size, and layer topology (vector of layer sizes, includes output & middle)
         NeuralNet(int, const std::vector<LayerDefinition>&);
+
         // Predict output
         std::vector<double> predict(const std::vector<double>&);
+
         // Mutate weightings in the neural net by an interval amount
         void mutate(double, double);
 
         //size of public layers
         int getInputSize();
+
         int getOutputSize();
+
+        friend std::ostream& operator<<(std::ostream& os, const NeuralNet& nn);
+
+        friend std::istream& operator>>(std::istream& is, NeuralNet& nn);
+
     private:
         int inputSize;
         std::vector<Layer> layers;
--- tst/Test.cpp
+++ tst/Test.cpp
@@ -5,20 +5,45 @@
 #include <iostream>
 #include "../src/PyreNet.h"
 
-int main() {
+void stressTest() {
     std::vector<PyreNet::LayerDefinition> layerDefs;
     layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
     layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
     layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
     layerDefs.emplace_back(2, PyreNet::LayerDefinition::activationType::relu);
     PyreNet::NeuralNet nn(5, layerDefs);
-    nn.mutate(0, 1);
+    nn.mutate(0, 0.1);
     std::vector<double> input{0,1,2,3,4};
+    std::cout << "Stress test complete" << std::endl;
+}
 
-    for (int i = 0; i < 100; ++i) {
-        for (double x : nn.predict(input))
-            std::cout << x << std::endl;
-    }
+void serializeTest() {
+    std::vector<PyreNet::LayerDefinition> layerDefs;
+    layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
+    layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
+    layerDefs.emplace_back(50, PyreNet::LayerDefinition::activationType::relu);
+    layerDefs.emplace_back(2, PyreNet::LayerDefinition::activationType::relu);
+    PyreNet::NeuralNet nn(5, layerDefs);
+    nn.mutate(0,2);
+    std::vector<double> input{0,1,2,3,4};
+    double output = nn.predict(input)[0];
 
+    std::stringstream ss;
+    ss << nn;
+
+    nn.mutate(2,2);
+    if (std::abs(nn.predict(input)[0] - output) <= 1)
+        exit(EXIT_FAILURE);
+
+    ss >> nn;
+    if (std::abs(nn.predict(input)[0] - output) > 1)
+        exit(EXIT_FAILURE);
+
+    std::cout << "Serialize test complete" << std::endl;
+}
+
+int main() {
+    stressTest();
+    serializeTest();
     return 0;
 }
